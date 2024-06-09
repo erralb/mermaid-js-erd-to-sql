749fd840ec0b2ab9f354708bb9d881c2bcd2ee72
Pierre Blarre <pierre.blarre@gmail.com>
2024-06-09 13:14:29 +0200

    Relationships only diagram demo

--- demos/relationships.sql
+++ demos/relationships.sql
@@ -1,37 +1,18 @@
 -- SQL script generated from Mermaid JS ERD to SQL
 -- Schema: MermaidERDToSQLRelationshipsTest
 
-CREATE TABLE Test1 (
-    pk11 INT,
-    pk12 INT,
-    PRIMARY KEY (pk11,pk12)
+CREATE TABLE Test1 
 );
 
-CREATE TABLE Test2 (
-    pk11 INT,
-    pk12 INT,
-    FOREIGN KEY(pk11, pk12) REFERENCES Test1(pk11, pk12)
+CREATE TABLE Test2 
 );
 
-CREATE TABLE Test3 (
-    pk11 INT,
-    pk12 INT,
-    FOREIGN KEY(pk11, pk12) REFERENCES Test1(pk11, pk12)
+CREATE TABLE Test3 
 );
 
 CREATE TABLE Test4 
 );
 
-CREATE TABLE Test1 (
-    pk11 INT,
-    pk12 INT,
-    PRIMARY KEY (pk11,pk12)
-);
-
-CREATE TABLE Test1_Test4_test14 (
-    pk11 INT,
-    pk12 INT,
-    PRIMARY KEY (pk11,pk12),
-    FOREIGN KEY(pk11, pk12) REFERENCES Test1(pk11, pk12)
+CREATE TABLE Test1_Test4_test14 
 );
 
