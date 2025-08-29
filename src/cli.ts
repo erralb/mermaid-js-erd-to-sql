54d1dc451308f41bfba46c9835aefefc04f07135
Erik Kranjec <erik.kranjec8@outlook.com>
2025-08-29 13:48:59 +0200

    Remove unnecesary node specification from cli.ts

--- src/cli.ts
+++ src/cli.ts
@@ -1,5 +1,3 @@
-#!/usr/bin/env node
-
 import * as convert from './convert/index';
 import yargs from 'yargs';
 import { hideBin } from 'yargs/helpers';
@@ -21,36 +19,30 @@ const argv = yargs(hideBin(process.argv))
   .parseSync();
 
 
-let convertFunction = null;
-switch (argv.flavor) {
-  case 'postgres':
-    convertFunction = convert.MermaidERDPostgreSQL.toSQL;
-    break;
-  case 'mysql':
-    convertFunction = convert.MermaidERDPostgreSQL.toSQL;
-    break;
-  case 'sqlite':
-    convertFunction = convert.MermaidERDPostgreSQL.toSQL;
-    break;
-  case 'sql':
-    convertFunction = convert.MermaidERDPostgreSQL.toSQL;
-    break;
-}
-
-if (convertFunction = null) {
-  console.error(`Unsupported SQL flavor: ${argv.flavor}.`);
-  process.exit(1);
-}
-
 const filePath = argv.file as string;
 const { outputFilename, markdownContent } = getMermaidFileContent(filePath);
 if (outputFilename === '' || markdownContent === '') {
   console.error("Error while reading Mermaid file content.");
   process.exit(1);
 }
+
 const { schema, entities, relationships } = convert.parseMermaidERD(markdownContent);
-const sqlScript = convert.MermaidERDSQL.toSQL(schema, entities, relationships);
-writeGeneratedFile(outputFilename, sqlScript);
+if (argv.flavor === 'postgres') {
+  const sqlScript = convert.MermaidERDPostgreSQL.toSQL(schema, entities, relationships);
+  writeGeneratedFile(outputFilename, sqlScript);
+} else if (argv.flavor === 'mysql') {
+  const sqlScript = convert.MermaidERDMySQL.toSQL(schema, entities, relationships);
+  writeGeneratedFile(outputFilename, sqlScript);
+} else if (argv.flavor === 'sqlite') {
+  const sqlScript = convert.MermaidERDSQLite.toSQL(schema, entities, relationships);
+  writeGeneratedFile(outputFilename, sqlScript);
+} else if (argv.flavor === 'sql') {
+  const sqlScript = convert.MermaidERDSQL.toSQL(schema, entities, relationships);
+  writeGeneratedFile(outputFilename, sqlScript);
+} else {
+  console.error(`Unsupported SQL flavor: ${argv.flavor}.`);
+  process.exit(1);
+}
 
 /**
  * Write the SQL script to a file
